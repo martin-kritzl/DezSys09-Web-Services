6e61517b1193e2e9cf69b725c4f45bf37df2e7df
mkritzl-tgm <mkritzl@student.tgm.ac.at>
2016-03-18 13:55:10 +0100

    Added Webconfiguration for database usage; Renamded endpoints; Added database connection properties

--- src/main/java/at/mkritzl/config/JerseyConfiguration.java
+++ src/main/java/at/mkritzl/config/JerseyConfiguration.java
@@ -1,7 +1,6 @@
 package at.mkritzl.config;
 
-import at.mkritzl.endpoints.LoginEndpoint;
-import at.mkritzl.endpoints.RegisterEndpoint;
+import at.mkritzl.endpoints.UserEndpoint;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Configuration;
@@ -10,9 +9,7 @@ import org.springframework.context.annotation.Configuration;
 public class JerseyConfiguration extends ResourceConfig {
 
     public JerseyConfiguration() {
-//        this.register(LoginEndpoint.class);
-        this.register(RegisterEndpoint.class);
-//        this.register(AccountEndpoint.class);
+        this.register(UserEndpoint.class);
         this.register(JacksonFeature.class);
     }
 }
---
+++ src/main/java/at/mkritzl/endpoints/UserEndpoint.java
@@ -0,0 +1,72 @@
+package at.mkritzl.endpoints;
+
+import at.mkritzl.data.Message;
+import at.mkritzl.data.UserAccount;
+import at.mkritzl.persistent.UserRepository;
+import at.mkritzl.utils.UserAccountValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Named;
+import javax.validation.Valid;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/")
+@Produces({MediaType.APPLICATION_JSON})
+@Named
+public class UserEndpoint {
+
+    @Autowired
+    private UserRepository repository;
+
+    @POST
+    @Path("/register")
+    public Response register(UserAccount requestAccount) {
+        if (UserAccountValidator.hasData(requestAccount)) {
+            //Alle Argumente vorhanden
+            if (this.repository.findOne(requestAccount.getEmail()) == null) {
+                //Anlegen des neuen Benutzers
+                UserAccount account = new UserAccount(requestAccount.getEmail(), requestAccount.getPassword());
+                this.repository.save(account);
+                //Erfolgreiche Response schicken
+                int status = Response.Status.CREATED.getStatusCode();
+                return Response.status(status).entity(new Message(status, "Created account with email " + account.getEmail())).build();
+            } else {
+                //Der Benutzer ist bereits vorhanden
+                int status = Response.Status.FORBIDDEN.getStatusCode();
+                return Response.status(status).entity(new Message(status, "The email " + requestAccount.getEmail() + " is already used by another")).build();
+            }
+        } else {
+            //Die Argumente sind invalid
+            int status = Response.Status.BAD_REQUEST.getStatusCode();
+            return Response.status(status).entity(new Message(status, "One or more account-arguments not set")).build();
+        }
+    }
+
+    @POST
+    @Path("/login")
+    public Response login(UserAccount requestAccount) {
+        if (UserAccountValidator.hasData(requestAccount)) {
+            //Alle Argumente vorhanden
+            UserAccount account = this.repository.findOne(requestAccount.getEmail());
+
+            if (account != null && account.getPassword().equals(requestAccount.getPassword())) {
+                //Erfolgreicher Login
+                int status = Response.Status.OK.getStatusCode();
+                return Response.status(status).entity(new Message(status, "Account with email " + account.getEmail() + " was logged in successfully")).build();
+            } else {
+                //Falsche Anmeldedaten
+                int status = Response.Status.FORBIDDEN.getStatusCode();
+                return Response.status(status).entity(new Message(status, "Invalid account credentials. Please try again")).build();
+            }
+        } else {
+            //Die Argumente sind invalid
+            int status = Response.Status.BAD_REQUEST.getStatusCode();
+            return Response.status(status).entity(new Message(status, "Missing account argument or a required field was not set")).build();
+        }
+    }
+}
